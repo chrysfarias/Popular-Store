d83333a8b573da5c093e8cbfddc71d8ec42652ce
Chrystian <chrystianfarias@hotmail.com>
2023-03-13 17:13:30 -0400

    Posicionamento com Bootstrap

--- 1530-springmvc-parte1-aula1/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ 1530-springmvc-parte1-aula1/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -19,6 +19,7 @@ public class HomeController {
 		pedido.setUrlProduto("https://www.amazon.com.br/Fender-144522581-Player-Stratocaster-HSS/dp/B07T675ZH1/ref=pd_ci_mcx_mh_mcx_views_0?pd_rd_w=LGRWH&content-id=amzn1.sym.5750b988-c722-40d9-8c9a-41af4853947c&pf_rd_p=5750b988-c722-40d9-8c9a-41af4853947c&pf_rd_r=S54F733VFQ7V5G3G60EV&pd_rd_wg=fXWKK&pd_rd_r=b442fb99-fd01-465a-af55-b83cf8faa460&pd_rd_i=B07T675ZH1");
 		pedido.setDescricao("Guitarra Legal");
 		
+		
 		List<Pedido> pedidos = Arrays.asList(pedido);
 		model.addAttribute("pedidos",pedidos); // enviando a lista para a view		
 		return "home";
