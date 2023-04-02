3b5f767a70d11b8cf12f179e6cbddd245fe2b968
Chrystian <chrystianfarias@hotmail.com>
2023-04-01 23:43:10 -0400

    estilizando o formulario de pedidos

--- 1530-springmvc-parte1-aula1/src/main/java/br/com/alura/mvc/mudi/controller/PedidoformularioController.java
+++ 1530-springmvc-parte1-aula1/src/main/java/br/com/alura/mvc/mudi/controller/PedidoformularioController.java
@@ -2,25 +2,41 @@ package br.com.alura.mvc.mudi.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.alura.mvc.mudi.dto.RequisicaoNovoPedido;
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 @RequestMapping("pedido")
 public class PedidoformularioController {
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
 	
 	
 	
 	@GetMapping("formulario")
-	public String formulario(Model model) {
+	public String formulario() {
 		
 		return "pedido/formulario";
 		
+	}
+	
+	@PostMapping ("novo")
+	
+		public String novo(RequisicaoNovoPedido requisicao) {
+		
+		Pedido pedido = requisicao.toPedido();
+		pedidoRepository.save(pedido);
+		
+		return  "hello";
 	}
 		
 	
---
+++ 1530-springmvc-parte1-aula1/src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,50 @@
+package br.com.alura.mvc.mudi.dto;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+
+public class RequisicaoNovoPedido {
+	
+	private String nomeProduto;
+	private String UrlProduto;
+	private String urlImagem;
+	private String descricao;
+	
+	
+	
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public String getUrlProduto() {
+		return UrlProduto;
+	}
+	public void setUrlProduto(String urlProduto) {
+		UrlProduto = urlProduto;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	public Pedido toPedido() {
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto(nomeProduto);
+		pedido.setDescricao(descricao);
+		pedido.setUrlImagem(urlImagem);
+		pedido.setUrlProduto(UrlProduto);		
+		return pedido;
+	}
+
+	
+}
